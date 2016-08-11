4276c2dfbe6693b93a698eee54063ef755ac12b5
liumingning <liumingning@126.com>
2016-08-11 23:27:04 +0800

    Initial commit

---
+++ src/main/java/org/mingning/App.java
@@ -0,0 +1,12 @@
+
+package org.mingning;
+
+/**
+ * Hello world!
+ */
+public class App{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("aaaaaaaaaa");
+    }
+}
